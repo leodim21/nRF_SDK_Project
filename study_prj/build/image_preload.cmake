2242317e505b1c37d6266276d5db33a692857ea2
LeoDim21 <leonov.d@ajax.system>
2023-02-14 04:42:30 +0100

    Upd ignore

--- study_prj/build/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "/Users/leodim21/Workbanch/Prog_Nordic/adc_spi_led_test_project/study_prj;/opt/nordic/ncs/v2.2.0/nrf;/opt/nordic/ncs/v2.2.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "/Users/leodim21/Workbanch/Prog_Nordic/adc_spi_led_test_project/study_prj/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/opt/nordic/ncs/toolchains/v2.2.0/bin/python3" CACHE INTERNAL "NCS child image controlled")
